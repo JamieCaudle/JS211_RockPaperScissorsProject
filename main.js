7de714cdda665bb1dacc0b05dd14142732579c1b
JamieCaudle <jamielcaudle@gmail.com>
2020-06-07 16:44:13 -0500

    initial changes

--- main.js
+++ main.js
@@ -1,31 +1,50 @@
-// uses strict mode so strings are not coerced, variables are not hoisted, etc... 
-'use strict';
+"use strict";
+const assert = require("assert");
 
-// brings in the assert module for unit testing
-const assert = require('assert');
-// brings in the readline module to access the command line
-const readline = require('readline');
-// use the readline module to print out to the command line
+const readline = require("readline");
+
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
 // the function that will be called by the unit test below
 const rockPaperScissors = (hand1, hand2) => {
+  if (hand1 === hand2) {
+    return "It's a tie!";
+  }
 
-  // Write code here
-  // Use the unit test to see what is expected
+  if (hand1 === "paper" && hand2 === "rock") {
+    return "Hand one wins!";
+  }
 
-}
+  if (hand1 === "paper" && hand2 === "scissors") {
+    return "Hand two wins!";
+  }
 
-// the first function called in the program to get an input from the user
-// to run the function use the command: node main.js
-// to close it ctrl + C
+  if (hand1 === "rock" && hand2 === "scissors") {
+    return "Hand one wins!";
+  }
+
+  if (hand1 === "rock" && hand2 === "paper") {
+    return "Hand two wins!";
+  }
+
+  if (hand1 === "scissors" && hand2 === "paper") {
+    return "Hand one wins!";
+  }
+
+  if (hand1 === "scissors" && hand2 === "rock") {
+    return "Hand two wins!";
+  }
+};
+
+rockPaperScissors();
+
 function getPrompt() {
-  rl.question('hand1: ', (answer1) => {
-    rl.question('hand2: ', (answer2) => {
-      console.log( rockPaperScissors(answer1, answer2) );
+  rl.question("hand1: ", (answer1) => {
+    rl.question("hand2: ", (answer2) => {
+      console.log(rockPaperScissors(answer1, answer2));
       getPrompt();
     });
   });
@@ -34,29 +53,24 @@ function getPrompt() {
 // Unit Tests
 // You use them run the command: npm test main.js
 // to close them ctrl + C
-if (typeof describe === 'function') {
-
-  // most are notes for human eyes to read, but essentially passes in inputs then compares if the function you built return the expected output.
-  describe('#rockPaperScissors()', () => {
-    it('should detect a tie', () => {
-      assert.equal(rockPaperScissors('rock', 'rock'), "It's a tie!");
-      assert.equal(rockPaperScissors('paper', 'paper'), "It's a tie!");
-      assert.equal(rockPaperScissors('scissors', 'scissors'), "It's a tie!");
+if (typeof describe === "function") {
+  describe("#rockPaperScissors()", () => {
+    it("should detect a tie", () => {
+      assert.equal(rockPaperScissors("rock", "rock"), "It's a tie!");
+      assert.equal(rockPaperScissors("paper", "paper"), "It's a tie!");
+      assert.equal(rockPaperScissors("scissors", "scissors"), "It's a tie!");
     });
-    it('should detect which hand won', () => {
-      assert.equal(rockPaperScissors('rock', 'paper'), "Hand two wins!");
-      assert.equal(rockPaperScissors('paper', 'scissors'), "Hand two wins!");
-      assert.equal(rockPaperScissors('rock', 'scissors'), "Hand one wins!");
+    it("should detect which hand won", () => {
+      assert.equal(rockPaperScissors("rock", "paper"), "Hand two wins!");
+      assert.equal(rockPaperScissors("paper", "scissors"), "Hand two wins!");
+      assert.equal(rockPaperScissors("rock", "scissors"), "Hand one wins!");
     });
     it('should scrub input to ensure lowercase with "trim"ed whitepace', () => {
-      assert.equal(rockPaperScissors('rOcK', ' paper '), "Hand two wins!");
-      assert.equal(rockPaperScissors('Paper', 'SCISSORS'), "Hand two wins!");
-      assert.equal(rockPaperScissors('rock ', 'sCiSsOrs'), "Hand one wins!");
+      assert.equal(rockPaperScissors("rOcK", " paper "), "Hand two wins!");
+      assert.equal(rockPaperScissors("Paper", "SCISSORS"), "Hand two wins!");
+      assert.equal(rockPaperScissors("rock ", "sCiSsOrs"), "Hand one wins!");
     });
   });
 } else {
-
-  // always returns ask the user for another input
   getPrompt();
-
 }
